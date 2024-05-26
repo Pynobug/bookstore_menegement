799bb4607416f0220492c208db9cbea19647fcfd
Pynobug <906713420@qq.com>
2024-05-26 17:48:34 +0800

    product

---
+++ src/main/java/com/fby/store/controller/ProductController.java
@@ -0,0 +1,2 @@
+package com.fby.store.controller;public class ProductController {
+}
---
+++ src/main/java/com/fby/store/service/IProductService.java
@@ -0,0 +1,11 @@
+package com.fby.store.service.impl;
+
+import com.fby.store.entity.Product;
+
+import java.util.List;
+
+public interface IProductService {
+    List<Product> findHostList();
+}
+
+
